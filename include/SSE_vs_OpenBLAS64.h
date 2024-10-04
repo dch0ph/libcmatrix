387b695f71f9df486aefea044fb6a1eb122ccb7f
dch0ph <paul.hodgkinson@durham.ac.uk>
2024-10-04 17:57:31 +0100

    OpenBLAS64 support

---
+++ include/SSE_vs_OpenBLAS64.h
@@ -0,0 +1,9 @@
+/* Internal / external cross-overs created by testops 1000 */
+#define LCM_INTERNAL_DMM 8
+#define LCM_INTERNAL_ZMM 6
+#define LCM_INTERNAL_ZTM 7
+#define LCM_INTERNAL_ZMT 8
+#define LCM_INTERNAL_DMLA 0
+#define LCM_INTERNAL_ZMLA 0
+#define LCM_INTERNAL_DMV 0
+#define LCM_INTERNAL_ZMV 0
