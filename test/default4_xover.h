485199b4c1c07d022f0e04cd5251bbed1a805831
dch0ph <paul.hodgkinson@durham.ac.uk>
2024-10-05 10:05:52 +0100

    Updates to complex + tidy
    Tidy. Remove register from complex

--- test/default4_xover.h
+++
@@ -1,9 +0,0 @@
-/* Internal / external cross-overs created by testops 300 */
-#define DMM 4
-#define ZMM 18
-#define ZTM 34
-#define ZMT 16
-#define DMLA 46
-#define ZMLA 90
-#define DMV 8
-#define ZMV 7
