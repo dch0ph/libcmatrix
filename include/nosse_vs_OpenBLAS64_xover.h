601ad2e429c8e23c210fd6043e6e3c7fc36eb372
dch0ph <paul.hodgkinson@durham.ac.uk>
2025-08-13 09:52:04 +0100

    Create nosse_vs_OpenBLAS64_xover.h

---
+++ include/nosse_vs_OpenBLAS64_xover.h
@@ -0,0 +1,9 @@
+/* Internal / external cross-overs created by testops 1000 */
+#define LCM_INTERNAL_DMM 8
+#define LCM_INTERNAL_ZMM 6
+#define LCM_INTERNAL_ZTM 6
+#define LCM_INTERNAL_ZMT 6
+#define LCM_INTERNAL_DMLA 8
+#define LCM_INTERNAL_ZMLA 4
+#define LCM_INTERNAL_DMV 8
+#define LCM_INTERNAL_ZMV 8
